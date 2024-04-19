356d4180481fb03fd0137db96163f12d1f3097aa
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-04-20 01:57:42 +0900

    learn: Users Microservice - AuthenticationFilter 추가
    + loadUserByUsername() 구현

--- 04-users-microservice-1/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ 04-users-microservice-1/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -4,12 +4,16 @@ import com.example.userservice.service.UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -17,28 +21,45 @@ public class WebSecurity {
 
     private UserService userService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
 
     private static final String[] WHITE_LIST = {
             "/users/**",
             "/**"
     };
 
-    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
     }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf(AbstractHttpConfigurer::disable);
         http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         http.authorizeHttpRequests(
             authorize -> authorize
-                    .requestMatchers(WHITE_LIST).permitAll()
+//                    .requestMatchers(WHITE_LIST).permitAll()
+                    .requestMatchers("/**").access(
+                            new WebExpressionAuthorizationManager(
+                                    "hasIpAddress('127.0.0.1') or hasIpAddress('192.168.0.7')"))
                     .requestMatchers(PathRequest.toH2Console()).permitAll()
         );
+        http.addFilter(getAuthenticationFilter(authenticationManager));
 
         return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+        return new AuthenticationFilter(authenticationManager, userService, env);
     }
 }
